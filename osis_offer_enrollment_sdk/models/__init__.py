f9b369a850288b612f237dbb3b998a60271f1cd2
Benjamin Daubry <benjamin.daubry@uclouvain.be>
2021-09-15 14:10:50 +0200

    build-1.0.0

---
+++ osis_offer_enrollment_sdk/models/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from osis_offer_enrollment_sdk.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from osis_offer_enrollment_sdk.model.accepted_language_enum import AcceptedLanguageEnum
+from osis_offer_enrollment_sdk.model.enrollment import Enrollment
+from osis_offer_enrollment_sdk.model.enrollment_list import EnrollmentList
+from osis_offer_enrollment_sdk.model.enrollment_list_all_of import EnrollmentListAllOf
+from osis_offer_enrollment_sdk.model.error import Error
+from osis_offer_enrollment_sdk.model.paging import Paging
